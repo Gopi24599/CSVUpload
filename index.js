91c2f6d81e081af19d0cf09901af517904823f6c
Gopi24599 <134787964+Gopi24599@users.noreply.github.com>
2023-07-07 21:29:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/** ------------------ IMPORTING PACKAGE ------------------ **/
+const express = require('express');
+const port = 8000;
+const app = express();
+const path = require('path');
+
+const expressLayouts = require('express-ejs-layouts');
+const csv = require('csv-parser');
+const db = require("./config/mongoose");
+const bodyParser = require('body-parser');
+
+app.use(expressLayouts);
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+
+//accesing static files from assets folder
+app.use(express.static('./assets'));    
+
+//setting up view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use('/', require('./routes'));
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log('Error', err);
+        return;
+    }
+    console.log('Server is up and running on port: ', port);
+
+});
